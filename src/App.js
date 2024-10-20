ab3c25155011be01ebee461e6ee5b45d9332624e
Ansh_13 <104825546+anshmendpara-13@users.noreply.github.com>
2024-10-20 17:25:47 +0530

    final

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ import Landing from './pages/Landing/Landing';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ToDoList from './pages/ToDo/ToDoList';
-import NotFound from './pages/NotFound/NotFound'; // Import a NotFound component
 import './App.css';
 import 'antd/dist/reset.css';
 
@@ -37,7 +36,6 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/to-do-list" element={<ToDoList />} />
-      <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
     </Routes>
   );
 }
